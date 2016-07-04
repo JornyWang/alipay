ea4c501bf65e8f8d46ccbf1adaf5ef250c910669
wangdan <dan.wang@678.ms>
2016-07-04 19:38:42 +0800

    change:即时到帐文档说明

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # alipay
 alipay nodejs 支付宝支付 即时到帐
 
-#attention !!
-此项目未验证,正在开发中。。。
+#install
+npm install alipay.wang
+
+#即时到账
+｀｀｀
+var mapi;
+
+mapi = require('../lib/mechant.alipay.server.utils');
+instance = new mapi() //instance = new mapi(config)
+var data = {
+  extra_common_param: 'test'
+  total_fee: '0.01'
+  subject: '测试商品'
+};
+var callback = function(err, result){
+  console.log(err);
+  console.log(result);
+};
+instance.get_direct_pay_url({data}, callback)
+
+｀｀｀
+
+#其他接口上线中。。
+
--- test.js
+++ test.js
@@ -1,42 +1,16 @@
 "use strict";
 
-var get_direct_url, iconv, isRunError, mapi;
+var mapi;
 
 mapi = require('../lib/mechant.alipay.server.utils');
 instance = new mapi() //instance = new mapi(config)
-iconv = require('iconv').Iconv;
-
-isRunError = function(body) {
-  var html;
-  html = new iconv('GBK', 'UTF-8').convert(body).toString();
-  return html.indexOf('错误代码') !== -1;
-};
-
-//获取即时到帐url
-get_direct_url = function(data) {
-  return instance.create_direct_pay_by_user(data, function(error, result) {
-    if (result.statusCode === 200 || result.statusCode === 302) {
-      if (result.body && isRunError(result.body)) {
-        console.log('支付宝接口调用失败(权限不足或者参数有误)');
-        return {
-          error: '支付宝接口调用失败(权限不足或者参数有误)'
-        };
-      } else {
-        return {
-          url: "https://" + result.request.host + result.request.path + "?" + result.request.body
-        };
-      }
-    } else {
-      console.log('支付宝接口出错');
-      return {
-        error: '支付宝接口出错'
-      };
-    }
-  });
-};
 var data = {
   extra_common_param: 'test'
   total_fee: '0.01'
   subject: '测试商品'
 };
-console.log(get_direct_url({data}));
+var callback = function(err, result){
+  console.log(err);
+  console.log(result);
+};
+instance.get_direct_pay_url({data}, callback)
