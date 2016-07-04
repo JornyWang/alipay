5975b38f7821e73640f8a9ebe168088211f34aee
wangdan <dan.wang@678.ms>
2016-07-04 19:52:09 +0800

    change:引用错误问题

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var mapi = require('./lib/mechant.alipay.server.utils');
-var openapi = require('./lib/mechant.alipay.server.utils');
+var openapi = require('./lib/open.alipay.server.utils');
+
+
 
 module.exports = {
   mapi: mapi,
